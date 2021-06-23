3c29b510761d921951f0fe846eabf8b7805835a7
alumja <77750709+alumja@users.noreply.github.com>
2021-06-23 17:38:35 +0200

    Update settings.py

--- orla_digital/orla_digital/settings.py
+++ orla_digital/orla_digital/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r(ofh^l2al071111lpzuhajxi1$me$49c!-dsoot%c7mtay7vl'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
